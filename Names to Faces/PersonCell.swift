cd612b841df73bacfc88a32f6e6eec4b82dd3567
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-19 09:50:22 +0300

    Created PersonCell class file

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Rinalds Domanovs on 19/06/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
